8d837244d70e59b1805e87c07c9893206d01a107
Feroan101 <feroanmothy666#gmail.com>
2026-01-02 13:42:14 +0530

    feat: added an assembler (py)

--- Cradle/assembler.py
+++ Cradle/assembler.py
@@ -13,15 +13,37 @@
 #JZ - 0x09
 #PRINT = 0x0A
 
-code = [
-    0x00,
-    0x00,
-    0x00,
-    0x01, 0x05,   # PUSH 5
-    0x0A,         # PRINT
-    0xFF
-]
+opcodes = {
+        "HALT": 0xFF,
+        "NOP": 0x00, 
+        "PUSH": 0x01, 
+        "POP": 0x02, 
+        "ADD": 0x03, 
+        "SUB": 0x04, 
+        "MUL": 0x05, 
+        "DIV": 0x06, 
+        "DUP": 0x07, 
+        "JMP": 0x08, 
+        "JZ": 0x09, 
+        "PRINT": 0x0A, 
+        }
 
+code = []
+
+with open("assembly.s", "r") as ass:
+    content = ass.read()
+
+
+Lines = content.split()
+for line in Lines:
+    if line in opcodes:
+        code.append(opcodes[line])
+    elif line.isnumeric():
+        code.append(int(line))
+    else:
+        print(line + " is a invalid instruction")
+    
+print(code)
 
 with open("program.bin", "wb") as f:
     f.write(bytes(code))
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ It’s designed to be simple, explicit, and run on its own virtual machine.
 Cradle is the *virtual machine* Enor runs on.
 It executes Enor bytecode and defines the runtime model.
 
-## status
+## Status
 
-Still *in development*.
-Things will evolve as the VM matures.
+-*Still in development*.
+-Things will evolve as the VM matures.
